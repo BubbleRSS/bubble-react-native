a1fc67c1120297443487762b39edb0558863644c
Mayhm <mayhm.social@proton.me>
2024-11-10 15:04:23 -0300

    feat: create StackNavigator

---
+++ src/presentation/components/StackNavigator/index.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { BottomTabNavigator } from "@/presentation/components/BottomTabNavigator";
+
+const Stack = createStackNavigator();
+
+export const StackNavigator: FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Tabs'
+          component={BottomTabNavigator}
+          options={{
+            headerShown: false
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
