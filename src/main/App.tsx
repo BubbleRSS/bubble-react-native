677723371867d9da67d68ad8275954c76cd37306
Mayhm <mayhm.social@proton.me>
2024-11-10 13:33:30 -0300

    refactor: add paper provider

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,7 +1,10 @@
 import { Router } from "@/presentation/components/Router";
+import { PaperProvider } from "react-native-paper";
 
 export default function App(): JSX.Element {
   return (
-    <Router />
+    <PaperProvider>
+      <Router />
+    </PaperProvider>
   )
 }
